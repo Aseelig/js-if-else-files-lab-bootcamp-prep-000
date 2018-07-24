41abc653a5a14ceb8f7f8c8172b088f8ef7eb46a
Alec Seelig <alec.seelig@flatironschool.com>
2018-07-24 16:24:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var name = "Joe"
 var height = "74"
 if (name === "susan") {
-  console.log ("the name
-  is correct")
+  console.log ("the name is correct")
 }
 // Don't worry about this
 module.exports = { name, height
